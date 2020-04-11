1fe9d716a98a6f6e5613ee34a2395baac83a407d
AxelGarciaTello <gata21022000@hotmail.com>
2020-04-11 15:24:48 -0500

    Clase padre de los menus

--- GUI/MenuFrame.java
+++ GUI/MenuFrame.java
@@ -1,14 +1,23 @@
 
 package GUI;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import static javax.swing.SwingConstants.CENTER;
 
 
 public class MenuFrame extends JFrame {
+    public Container contenedor;
+    public JLabel icono;
     
     public MenuFrame(){
-        super("Matematicas con señas");
+        super("Mate con señas");
         initComponents();
     }
     
@@ -22,6 +31,31 @@ public class MenuFrame extends JFrame {
                         getClass().getResource("Iconos/Icono.png")
                 ).getImage()
         );
+        JLabel titulo = new JLabel("Mate con señas");
+        titulo.setFont(new Font("Ubuntu", 0, 35));
+        titulo.setForeground(new Color(255, 255, 255));
+        titulo.setHorizontalAlignment(CENTER);
+        titulo.setVerticalAlignment(CENTER);
+        icono = new JLabel();
+        icono.setIcon(
+                new ImageIcon(
+                        getClass().getResource("Iconos/IconoGrande.png")
+                )
+        );
+        icono.setHorizontalAlignment(CENTER);
+        icono.setVerticalAlignment(CENTER);
+        contenedor = this.getContentPane();
+        contenedor.setBackground(new Color(0, 80, 0));
+        contenedor.setLayout(new BorderLayout());
+        contenedor.add(titulo, BorderLayout.NORTH);
+    }
+    
+    public void agregarMenu(JPanel menu){
+        contenedor.add(menu, BorderLayout.WEST);
+    }
+    
+    public void agregarFondo(JPanel fondo){
+        contenedor.add(fondo, BorderLayout.CENTER);
     }
     
 }
