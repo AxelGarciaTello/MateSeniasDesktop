43cc8141e93bb5d0779c864567f4a9cce24b94e9
AxelGarciaTello <gata21022000@hotmail.com>
2020-04-11 12:07:50 -0500

    Incluye el metdo main

---
+++ MateSenias.java
@@ -0,0 +1,14 @@
+
+import GUI.IngresoNinio;
+
+
+
+
+public class MateSenias {
+
+    public static void main(String[] args) {
+        IngresoNinio ingreso = new IngresoNinio();
+        
+    }
+    
+}
