ccb7c2e76bd1ce4a9e370500aadb9852491a5a89
AxelGarciaTello <gata21022000@hotmail.com>
2020-04-15 09:02:21 -0500

    Aplicación de destructores

--- Control/IngresarNinioActionListener.java
+++ Control/IngresarNinioActionListener.java
@@ -2,25 +2,26 @@
 package Control;
 
 import GUI.IngresoNinio;
-import GUI.IngresoTutor;
+import GUI.MenuNinio;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 
 
-public class IrIngresoNinioActionListener implements ActionListener {
+
+public class IngresarNinioActionListener implements ActionListener{
     private JFrame ventana;
     
-    public IrIngresoNinioActionListener(JFrame ventana){
+    public IngresarNinioActionListener(JFrame ventana){
         this.ventana=ventana;
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        IngresoNinio ninio = new IngresoNinio();
-        ninio.setVisible(true);
-        if(ventana instanceof IngresoTutor){
-            ((IngresoTutor)ventana).destruir();
+        MenuNinio menu = new MenuNinio();
+        menu.setVisible(true);
+        if(ventana instanceof IngresoNinio){
+            ((IngresoNinio)ventana).destruir();
         }
         ventana.dispose();
     }
--- Control/IrIngresoTutorActionListener.java
+++ Control/IrIngresoTutorActionListener.java
@@ -1,22 +1,27 @@
 
 package Control;
 
+import GUI.IngresoNinio;
 import GUI.IngresoTutor;
-import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JFrame;
 
 
 public class IrIngresoTutorActionListener implements ActionListener {
-    private Frame ventana;
+    private JFrame ventana;
     
-    public IrIngresoTutorActionListener(Frame ventana){
+    public IrIngresoTutorActionListener(JFrame ventana){
         this.ventana=ventana;
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
         IngresoTutor tutor = new IngresoTutor();
+        tutor.setVisible(true);
+        if(ventana instanceof IngresoNinio){
+            ((IngresoNinio)ventana).destruir();
+        }
         ventana.dispose();
     }
     
