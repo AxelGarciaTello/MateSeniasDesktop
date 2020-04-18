5a9d044f40ae6e37d00ebc70eb4eec7260a6a979
AxelGarciaTello <gata21022000@hotmail.com>
2020-04-18 18:51:56 -0500

    Cambio de inicio para pruebas

--- MateSenias.java
+++ MateSenias.java
@@ -1,15 +1,13 @@
 
-import GUI.EjemploFrame;
-import GUI.Ejercicio;
 import GUI.IngresoNinio;
+import GUI.JuegoFrame;
+import GUI.OpcionesFrame;
 
 public class MateSenias {
 
     public static void main(String[] args) {
         IngresoNinio ingreso = new IngresoNinio();
         ingreso.setVisible(true);
-        Ejercicio ef= new Ejercicio("Ejemplo","+");
-        ef.setVisible(true);
     }
     
 }
