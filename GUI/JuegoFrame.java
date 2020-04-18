d4107b76b665f9765d901b0b9e6abab138790f6e
AxelGarciaTello <gata21022000@hotmail.com>
2020-04-18 18:50:32 -0500

    Clase Juego para mostrar crucigramas

---
+++ GUI/JuegoFrame.java
@@ -0,0 +1,127 @@
+
+package GUI;
+
+import Control.CerrarVentanaActionListener;
+import Control.VerOpcionesActionListener;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import static javax.swing.SwingConstants.CENTER;
+
+
+public class JuegoFrame extends JFrame {
+    private JButton atras;
+    private JButton calificar;
+    private JButton[] crucigrama;
+    private Container contenedor;
+    
+    public JuegoFrame(String titulo){
+        initComponents(titulo);
+    }
+    
+    private void initComponents(String titulo){
+        int x=0;
+        this.setSize(930, 500);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setIconImage(
+                new ImageIcon(
+                        getClass().getResource("Iconos/Icono.png")
+                ).getImage()
+        );
+        this.setUndecorated(true);
+        contenedor=this.getContentPane();
+        contenedor.setBackground(new Color(0, 80, 0));
+        contenedor.setLayout(null);
+        JLabel etiqueta = new JLabel("Crucigrama: "+titulo);
+        etiqueta.setSize(930, 35);
+        etiqueta.setLocation(0, 10);
+        etiqueta.setFont(new Font("Ubuntu", 0, 35));
+        etiqueta.setForeground(new Color(255, 255, 255));
+        etiqueta.setHorizontalAlignment(CENTER);
+        etiqueta.setVerticalAlignment(CENTER);
+        contenedor.add(etiqueta);
+        atras = new JButton();
+        atras.setSize(50, 50);
+        atras.setIcon(
+                new ImageIcon(
+                        getClass().getResource("Iconos/Atras.png")
+                )
+        );
+        atras.setLocation(10, 10);
+        atras.addActionListener(
+                new CerrarVentanaActionListener(this)
+        );
+        contenedor.add(atras);
+        crucigrama = new JButton[24];
+        for(x=0; x<24; x++){
+            crucigrama[x] = new JButton("000");
+            crucigrama[x].setSize(75, 75);
+            crucigrama[x].setHorizontalAlignment(JTextField.CENTER);
+        }
+        crucigrama[0].setLocation(275,100);
+        crucigrama[1].setLocation(575,100);
+        crucigrama[2].setLocation(200,175);
+        crucigrama[3].setLocation(275,175);
+        crucigrama[4].setLocation(350,175);
+        crucigrama[5].setLocation(425,175);
+        crucigrama[6].setLocation(500,175);
+        crucigrama[7].setLocation(575,175);
+        crucigrama[8].setLocation(650,175);
+        crucigrama[9].setLocation(725,175);
+        crucigrama[10].setLocation(800,175);
+        crucigrama[11].setLocation(275,250);
+        crucigrama[12].setLocation(575,250);
+        crucigrama[13].setLocation(50,325);
+        crucigrama[14].setLocation(125,325);
+        crucigrama[15].setLocation(200,325);
+        crucigrama[16].setLocation(275,325);
+        crucigrama[17].setLocation(350,325);
+        crucigrama[18].setLocation(425,325);
+        crucigrama[19].setLocation(500,325);
+        crucigrama[20].setLocation(575,325);
+        crucigrama[21].setLocation(650,325);
+        crucigrama[22].setLocation(275,400);
+        crucigrama[23].setLocation(575,400);
+        for(x=0; x<24; x++){
+            crucigrama[x].addActionListener(
+                    new VerOpcionesActionListener(crucigrama[x])
+            );
+            contenedor.add(crucigrama[x]);
+        }
+        calificar = new JButton();
+        calificar.setSize(50, 50);
+        calificar.setIcon(
+                new ImageIcon(
+                        getClass().getResource("Iconos/Atras.png")
+                )
+        );
+        calificar.setLocation(870,10);
+        contenedor.add(calificar);
+    }
+    
+    public void destruir(){
+        int x=0;
+        if(atras!=null){
+            atras=null;
+        }
+        if(calificar!=null){
+            calificar=null;
+        }
+        for(x=0; x<14; x++){
+            if(crucigrama[x]!=null){
+                crucigrama[x]=null;
+            }
+        }
+        if(contenedor!=null){
+            contenedor=null;
+        }
+        System.gc();
+    }
+}
---
+++ GUI/OpcionesFrame.java
@@ -0,0 +1,92 @@
+
+package GUI;
+
+import Control.ColocarOpcionActionListener;
+import java.awt.Color;
+import java.awt.Container;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class OpcionesFrame extends JFrame {
+    private JButton proveniente;
+    private JButton[] opciones;
+    private Container contenedor;
+    
+    public OpcionesFrame(int x, int y, JButton proveniente){
+        this.proveniente=proveniente;
+        initComponents(x, y);
+    }
+    
+    private void initComponents(int x, int y){
+        int a;
+        this.setSize(170,330);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setLocation(x ,y);
+        this.setIconImage(
+                new ImageIcon(
+                        getClass().getResource("Iconos/Icono.png")
+                ).getImage()
+        );
+        this.setUndecorated(true);
+        contenedor=this.getContentPane();
+        contenedor.setBackground(new Color(0, 80, 0));
+        contenedor.setLayout(null);
+        opciones = new JButton[15];
+        opciones[0] = new JButton("+");
+        opciones[0].setLocation(10,10);
+        opciones[1] = new JButton("-");
+        opciones[1].setLocation(60,10);
+        opciones[2] = new JButton("x");
+        opciones[2].setLocation(10,60);
+        opciones[3] = new JButton("/");
+        opciones[3].setLocation(60,60);
+        opciones[4] = new JButton("=");
+        opciones[4].setLocation(110,10);
+        opciones[5] = new JButton("7");
+        opciones[5].setLocation(10,120);
+        opciones[6] = new JButton("8");
+        opciones[6].setLocation(60,120);
+        opciones[7] = new JButton("9");
+        opciones[7].setLocation(110,120);
+        opciones[8] = new JButton("4");
+        opciones[8].setLocation(10,170);
+        opciones[9] = new JButton("5");
+        opciones[9].setLocation(60,170);
+        opciones[10] = new JButton("6");
+        opciones[10].setLocation(110,170);
+        opciones[11] = new JButton("1");
+        opciones[11].setLocation(10,220);
+        opciones[12] = new JButton("2");
+        opciones[12].setLocation(60,220);
+        opciones[13] = new JButton("3");
+        opciones[13].setLocation(110,220);
+        opciones[14] = new JButton("0");
+        opciones[14].setLocation(10,270);
+        for(a=0; a<15; a++){
+            opciones[a].setSize(50,50);
+            opciones[a].addActionListener(
+                    new ColocarOpcionActionListener(
+                            proveniente, opciones[a].getText(), this
+                    )
+            );
+            contenedor.add(opciones[a]);
+        }
+    }
+    
+    public void destruir(){
+        int x=0;
+        for(x=0; x<15; x++){
+            if(opciones[x]!=null){
+                opciones[x]=null;
+            }
+        }
+        if(contenedor!=null){
+            contenedor=null;
+        }
+        System.gc();
+    }
+    
+}
