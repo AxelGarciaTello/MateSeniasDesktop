4dee12cfa7d3b5f423d554672b3c5f5a61042318
AxelGarciaTello <gata21022000@hotmail.com>
2020-04-28 08:16:55 -0500

    Cambio de inicio para pruebas

--- MateSenias.java
+++ MateSenias.java
@@ -1,14 +1,11 @@
 
 import GUI.IngresoNinio;
-import GUI.SistemaNumericoFrame;
 
 public class MateSenias {
 
     public static void main(String[] args) {
         IngresoNinio ingreso = new IngresoNinio();
         ingreso.setVisible(true);
-        SistemaNumericoFrame sn = new SistemaNumericoFrame();
-        sn.setVisible(true);
     }
     
 }
