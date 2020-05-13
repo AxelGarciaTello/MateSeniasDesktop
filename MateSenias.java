5b54cb91b092df0029ef7d30ea47c6406563caa7
AxelGarciaTello <gata21022000@hotmail.com>
2020-05-13 08:17:05 -0500

    Cambio en el inicio para pruebas

--- MateSenias.java
+++ MateSenias.java
@@ -1,14 +1,11 @@
 
 import GUI.IngresoNinio;
-import GUI.MenuTutor;
 
 public class MateSenias {
 
     public static void main(String[] args) {
         IngresoNinio ingreso = new IngresoNinio();
         ingreso.setVisible(true);
-        MenuTutor menu = new MenuTutor();
-        menu.setVisible(true);
     }
     
 }
