d0f970eac5117ad8f89e4c8e8bd8811b853f14d8
AxelGarciaTello <gata21022000@hotmail.com>
2020-05-14 08:13:00 -0500

    Cambios para pruebas

--- Control/IngresarTutorActionListener.java
+++ Control/IngresarTutorActionListener.java
@@ -4,6 +4,7 @@ package Control;
 import GUI.IngresoTutor;
 import GUI.MenuTutor;
 import GUI.RegistroFrame;
+import Logico.Ninio;
 import Logico.Tutor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,7 +20,10 @@ public class IngresarTutorActionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        MenuTutor menu = new MenuTutor(new Tutor("Axel","a","a",null));
+        Ninio[] ninios = new Ninio[2];
+        ninios[0] = new Ninio("Juan");
+        ninios[1] = new Ninio("Martha");
+        MenuTutor menu = new MenuTutor(new Tutor("Axel","a","a",ninios));
         menu.setVisible(true);
         if(ventana instanceof IngresoTutor){
             ((IngresoTutor)ventana).destruir();
