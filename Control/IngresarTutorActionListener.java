30c60319bad98dd142d2e93138d74aa816a79442
AxelGarciaTello <gata21022000@hotmail.com>
2020-06-11 08:02:51 -0500

    Carga de datos desde la BD para ingresar

--- Control/IngresarTutorActionListener.java
+++ Control/IngresarTutorActionListener.java
@@ -3,35 +3,50 @@ package Control;
 
 import GUI.IngresoTutor;
 import GUI.MenuTutor;
-import GUI.RegistroFrame;
-import Logico.Ninio;
 import Logico.Tutor;
+import Persistencia.IngresoTutorConexionBD;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 
 public class IngresarTutorActionListener implements ActionListener {
     private JFrame ventana;
+    private JTextField correo;
+    private JPasswordField contrasenia;
     
-    public IngresarTutorActionListener(JFrame ventana){
+    public IngresarTutorActionListener(JFrame ventana, JTextField correo,
+            JPasswordField contrasenia){
         this.ventana=ventana;
+        this.correo=correo;
+        this.contrasenia=contrasenia;
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        Ninio[] ninios = new Ninio[2];
-        ninios[0] = new Ninio("Juan");
-        ninios[1] = new Ninio("Martha");
-        MenuTutor menu = new MenuTutor(new Tutor("Axel","a","a",ninios));
-        menu.setVisible(true);
-        if(ventana instanceof IngresoTutor){
-            ((IngresoTutor)ventana).destruir();
+        Tutor tutor;
+        IngresoTutorConexionBD ingreso;
+        if(correo.getText().equals("") || contrasenia.getText().equals("")){
+            JOptionPane.showMessageDialog(
+                    null, "Por favor ingrese todos los datos", 
+                    "Datos incompletos", 2
+            );
         }
-        else if(ventana instanceof RegistroFrame){
-            ((RegistroFrame)ventana).destruir();
+        else{
+            ingreso = new IngresoTutorConexionBD();
+            tutor=ingreso.ingresarTutor(
+                    correo.getText(), contrasenia.getText()
+            );
+            if(tutor!=null){
+                MenuTutor menu = new MenuTutor(tutor);
+                menu.setVisible(true);
+                ((IngresoTutor)ventana).destruir();
+                ventana.dispose();
+            }
         }
-        ventana.dispose();
     }
     
 }
